60528026f8a893641a1f629c9a1c2e7e15b27862
diogotiago04 <diogo.santos@ctec.ufal.br>
2025-10-14 21:46:49 -0300

    Add files via upload

---
+++ ThermalFVT_mean_field.m
@@ -0,0 +1,105 @@
+
+function ThermalFVT_mean_field(nx,ny,frac,k_m,k_i)
+
+
+
+R = sqrt(frac*nx*ny/pi);                       % radius of circular material heterogeneity
+
+
+
+% Subvolume faces
+[i,j] = meshgrid(1:nx,1:ny);
+s = (i+(j-1)*nx)';
+faces = [s(:),s(:)+nx*ny+1,s(:)+nx,s(:)+nx*ny];
+faces(end-nx+1:end,3) = faces(1:nx,1);
+faces(nx:nx:end,2) = faces(1:nx:end-nx+1,4);
+
+
+% Degrees of freedom
+ndof = max(faces(:));
+dofIS = unique([faces(1:nx, 1); faces(end-nx+1:end, 3)]);
+dofIS = [dofIS(1),dofIS(end)];
+dofDE = unique([faces(nx:nx:end, 2); faces(1:nx:end-nx+1, 4)]);
+dofDE = [dofDE(1),dofDE(end)];
+
+
+% Degrees of freedom: fixed and free
+fixed = [dofIS dofDE];
+free = setdiff(1:ndof,fixed);
+
+
+% Sparse mapping indices
+iK = reshape(kron(faces,ones(4,1))',16*nx*ny,1);
+jK = reshape(kron(faces,ones(1,4))',16*nx*ny,1);
+iF = repmat((faces)',2,1);                
+jF = [ones(4,nx*ny); 2*ones(4,nx*ny)];
+
+
+
+%____________________________________LOCAL CONDUCTIVITY MATRIX AND HEAT FLUX VECTOR
+
+
+k0 = eye(2);
+
+% Auxiliary matrices
+a = ones(4,1);
+N1 = [0,-1]; N2 = [1,0]; N3 = [0,1]; N4 = [-1,0];
+N = [N1,zeros(1,6); zeros(1,2),N2,zeros(1,4);
+    zeros(1,4),N3,zeros(1,2); zeros(1,6),N4];
+A = [0 -1/2 0 1/4; 1/2 0 1/4 0;
+    0 1/2 0 1/4; -1/2 0 1/4 0];
+E = [0 0 0 0; 0 -1 0 3/2; -1 0 -3/2 0; 0 0 0 0;
+    0 0 0 0; 0 -1 0 -3/2; -1 0 3/2 0; 0 0 0 0];
+B = N*eye(8)*E;
+ab = (B*(A\a))\(B/A);
+Ab = A\(eye(4)-a*ab);
+
+% Local conductivy matrix
+K0 = B*Ab;
+
+% Local heat flux vector
+H0 = [N1;N2;N3;N4]*k0;
+
+% Temperature gradient operator
+B = [0, 1, 0, -1; -1, 0, 1, 0];
+
+x = InitialMaterialDesign(nx,ny,R,k_m,k_i);
+
+% FINITE-VOLUME THEORY ANALYSIS
+
+% Interpolation
+sK = K0(:)*x(:)';
+sF = H0(:)*x(:)';
+
+% Assembly global conductivy matrix
+K = sparse(iK, jK, sK, ndof, ndof); K = (K+K')/2;
+
+% Assembly of heat flux vectors corresponding to two unit temperature gradient tests
+Q0 = sparse(iF(:), jF(:), sF, ndof, 2);
+
+% Compute fluctuating temperatures for two unit temperature gradient tests
+T_til(free,:) = K(free,free) \ Q0(free,:);
+
+% HOMOGENIZATION BASED ON MEAN-FIELD THEORY
+G0 = eye(2);
+K_ast = zeros(2);
+for j = 1:ny
+    for i = 1:nx
+        s = i+(j-1)*nx;
+        A = G0+B*T_til(faces(s,:),:);
+        K_ast = K_ast+x(s)*A/(nx*ny);
+    end
+end
+end
+
+%___________________________________________________INITIAL MATERIAL DESIGN
+function x = InitialMaterialDesign(nx,ny,R,k_m,k_i)
+x = k_m*ones(nx,ny);
+for j = 1:ny
+    for i = 1:nx
+        if sqrt((i-nx/2-0.5)^2 + (j- ny/2-0.5)^2) < R
+            x(i,j) = k_i;
+        end
+    end
+end
+end
